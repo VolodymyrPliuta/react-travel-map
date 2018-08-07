3e49be3b4e5593c08c94f1f3f85adedb7d421f21
americanman <snowboardracer7@gmail.com>
2018-08-07 17:03:10 -0400

    Add a side bar from 'google-maps-react' package sample

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,13 @@ import { GoogleApiWrapper } from 'google-maps-react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <a className="menu" tabIndex="0">
+          <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+          </svg>
+        </a>
+        <h1 className="heading"> Google Maps API + React </h1>
         <Map  google={this.props.google}/>
       </div>
     );
