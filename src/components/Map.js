41cc1efdd96528749181971c10c782689a6b62d1
americanman <snowboardracer7@gmail.com>
2018-08-30 22:00:08 -0400

    Add filter to locations,markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,31 +5,32 @@ export default class Map extends Component {
 
    state = {
      locations: [
-       {name: 'Atlanta', location: {lat: 33.774483, lng: -84.382849}},
-       {name: 'Belize', location: {lat: 17.490481,  lng: -88.202213}},
-       {name: 'Cancun', location: {lat: 21.165197, lng: -86.827264}},
-       {name: 'Cape Canaveral', location: {lat: 28.392157, lng: -80.596978}},
-       {name: 'Costa Maya', location: {lat: 18.735196, lng:  -87.691022}},
-       {name: 'Cozumel', location: {lat: 20.508578, lng: -86.947737}},
-       {name: 'Dallas', location: {lat: 32.838814, lng: -96.786518}},
-       {name: 'Grand Turk', location: {lat: 21.428566, lng: -71.143985}},
-       {name: 'Green Cove Springs', location: {lat: 29.991212, lng: -81.689631}},
-       {name: 'Jacksonville', location: {lat: 30.274438, lng:  -81.388347}},
-       {name: 'Miami', location: {lat: 25.774763, lng: -80.130467}},
-       {name: 'Nassau', location: {lat: 25.078643, lng:  -77.338089}},
-       {name: 'New York', location: {lat: 40.736701, lng: -73.989334}},
-       {name: 'Nocatee', location: {lat: 30.104096, lng:  -81.430318}},
-       {name: 'Orlando', location: {lat: 28.546863, lng: -81.373917}},
-       {name: 'Roatan', location: {lat: 16.357849, lng:  -86.442765}},
-       {name: 'Saint-Martin', location: {lat: 18.0308266, lng: -63.0736329}},
-       {name: 'St Augustine', location: {lat: 29.906616, lng: -81.314784}},
-       {name: 'St Thomas', location: {lat: 18.339866, lng: -64.9249165}},
-       {name: 'Washington D.C.', location: {lat: 38.890270, lng: -77.008907}}
+       {name: 'Atlanta', location: {lat: 33.774483, lng: -84.382849}, visible: true},
+       {name: 'Belize', location: {lat: 17.490481,  lng: -88.202213}, visible: true},
+       {name: 'Cancun', location: {lat: 21.165197, lng: -86.827264}, visible: true},
+       {name: 'Cape Canaveral', location: {lat: 28.392157, lng: -80.596978}, visible: true},
+       {name: 'Costa Maya', location: {lat: 18.735196, lng:  -87.691022}, visible: true},
+       {name: 'Cozumel', location: {lat: 20.508578, lng: -86.947737}, visible: true},
+       {name: 'Dallas', location: {lat: 32.838814, lng: -96.786518}, visible: true},
+       {name: 'Grand Turk', location: {lat: 21.428566, lng: -71.143985}, visible: true},
+       {name: 'Green Cove Springs', location: {lat: 29.991212, lng: -81.689631}, visible: true},
+       {name: 'Jacksonville', location: {lat: 30.274438, lng:  -81.388347}, visible: true},
+       {name: 'Miami', location: {lat: 25.774763, lng: -80.130467}, visible: true},
+       {name: 'Nassau', location: {lat: 25.078643, lng:  -77.338089}, visible: true},
+       {name: 'New York', location: {lat: 40.736701, lng: -73.989334}, visible: true},
+       {name: 'Nocatee', location: {lat: 30.104096, lng:  -81.430318}, visible: true},
+       {name: 'Orlando', location: {lat: 28.546863, lng: -81.373917}, visible: true},
+       {name: 'Roatan', location: {lat: 16.357849, lng:  -86.442765}, visible: true},
+       {name: 'Saint-Martin', location: {lat: 18.0308266, lng: -63.0736329}, visible: true},
+       {name: 'St Augustine', location: {lat: 29.906616, lng: -81.314784}, visible: true},
+       {name: 'St Thomas', location: {lat: 18.339866, lng: -64.9249165}, visible: true},
+       {name: 'Washington D.C.', location: {lat: 38.890270, lng: -77.008907}, visible: true}
      ],
      markers: [],
      infowindow: new this.props.google.maps.InfoWindow(),
      map: null,
      bounds: null,
+     defaultMarkers: null,
      defaultLocations: null
   }
 
@@ -52,6 +53,7 @@ export default class Map extends Component {
       const {infowindow} = this.state;
       const bounds = new maps.LatLngBounds();
 
+
       this.setState({
         defaultLocations: this.state.locations
       })
@@ -69,29 +71,40 @@ export default class Map extends Component {
           that.populateInfoWindow(marker, infowindow)
         })
       })
-        input.addEventListener('keyup', (e) => {
-          let input = e.target.value
-          let filterResult = this.state.locations.filter((location) => {
-            if(location.name.indexOf(input) >= 0) {
-              return true
-            }
-            return false
-          })
-          if(input == 0){
-            this.setState({
-              locations: this.state.defaultLocations
-            })
+      input.addEventListener('keyup', (e) => {
+        let input = e.target.value
+        let locations = this.state.locations.map((location) => {
+
+          // Check if current marker matches criteria (user input)
+          const isVisible = location.name.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+          // Ensure marker is defined before performing any operations on it
+          if(location){
+            location.visible = isVisible
           }
-          else{
-          this.setState({
-            locations: filterResult
-          })
-          }
+          return location;
         })
+        this.setState({
+          locations
+        })
+        // Iterate over markers to we can determine if they should remain visible
+        let markers = this.state.markers.map((marker) => {
+          // Check if current marker matches criteria (user input)
+          const isVisible = marker.title.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+          // Ensure marker is defined before performing any operations on it
+          if(marker){
+            marker.setVisible(isVisible)
+          }
+          return marker;
+        })
+        this.setState({
+          markers
+        })
+      })
       map.fitBounds(bounds)
       this.setState({
         map,
         markers,
+        defaultMarkers: markers,
         bounds
       })
     }
@@ -152,9 +165,10 @@ export default class Map extends Component {
             <input role="search" type="text" value={this.state.value} onChange={this.handleValueChange} id="search-input"/>
             <ul className="locations-list">
               {
-                locations.map((m, i) =>
-                  <li key={i}>{m.name}</li>
-                )
+                locations.map((m, i) => {
+                  if(m.visible){
+                    return  <li key={i}>{m.name}</li>
+                  }})
               }
             </ul>
           </div>
